00c03c2d6e604e7d71497ec29442608f790cf3b7
HARUYAMA Seigo <haruyama@unixuser.org>
2013-06-30 16:44:21 +0900

    remove unused plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,9 +2,7 @@
                    [lein-difftest "2.0.0"]
                    [lein-pprint   "1.1.1"]
                    [lein-localrepo "0.4.1"]
-                   [lein-licenses "0.1.1"]
-                   [jonase/eastwood "0.0.2"]
-                   [lein-clojars "0.9.1"]]
+                   [lein-licenses "0.1.1"]]
          :search-page-size 30
          ; :repl-options  {:prompt  (fn  [ns]  (str "your command, master? "))}
          :jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]}}
