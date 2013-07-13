a40094a1b5de4c74a27834772ac7f58fb67daeba
HARUYAMA Seigo <haruyama@unixuser.org>
2013-07-13 23:21:06 +0900

    add lein-try

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,6 +2,7 @@
                    [lein-difftest "2.0.0"]
                    [lein-pprint   "1.1.1"]
                    [lein-localrepo "0.4.1"]
+                   [lein-try "0.1.1"]
                    [lein-licenses "0.1.1"]]
          :search-page-size 30
          ; :repl-options  {:prompt  (fn  [ns]  (str "your command, master? "))}
