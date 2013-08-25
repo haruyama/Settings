23b1dfd691dc5725508aa528cced2950802da931
HARUYAMA Seigo <haruyama@unixuser.org>
2013-08-25 10:34:36 +0900

    add lein-ancient

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,10 @@
 {:user  {:plugins [[lein-kibit "0.0.8"]
                    [lein-difftest "2.0.0"]
                    [lein-pprint   "1.1.1"]
-                   [lein-localrepo "0.4.1"]
-                   [lein-try "0.1.1"]
-                   [lein-licenses "0.1.1"]]
+                   [lein-localrepo "0.5.2"]
+                   [lein-try "0.3.0"]
+                   [lein-licenses "0.1.1"]
+                   [lein-ancient "0.4.4"]]
          :search-page-size 30
          ; :repl-options  {:prompt  (fn  [ns]  (str "your command, master? "))}
          :jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]}}
