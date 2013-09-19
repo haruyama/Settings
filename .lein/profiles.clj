bda828621301732476bc824a8e3db4d1bd2eaf07
HARUYAMA Seigo <seigo.haruyama@mixi.co.jp>
2013-09-19 19:43:58 +0900

    upgrade lein-try

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,7 @@
                    [lein-difftest "2.0.0"]
                    [lein-pprint   "1.1.1"]
                    [lein-localrepo "0.5.2"]
-                   [lein-try "0.3.0"]
+                   [lein-try "0.3.1"]
                    [lein-licenses "0.1.1"]
                    [lein-ancient "0.4.4"]]
          :search-page-size 30
