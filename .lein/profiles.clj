cd85a0fd418b7bf2cd4bd290dfe9cdedcd254a4c
HARUYAMA Seigo <haruyama@unixuser.org>
2013-11-08 11:15:19 +0900

    update versions

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
-{:user  {:plugins [[lein-ancient "0.4.4"]
+{:user  {:plugins [[lein-ancient "0.5.2"]
                    [lein-bikeshed "0.1.3"]
                    [lein-difftest "2.0.0"]
                    [lein-licenses "0.1.1"]
                    [lein-localrepo "0.5.2"]
                    [lein-kibit "0.0.8"]
                    [lein-pprint   "1.1.1"]
-                   [lein-try "0.3.2"] ]
+                   [lein-try "0.4.0"] ]
          :search-page-size 30
          ; :repl-options  {:prompt  (fn  [ns]  (str "your command, master? "))}
          :jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]
