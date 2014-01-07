50d47b72251af71dc68efe559f002d32cc8ec74d
HARUYAMA Seigo <haruyama@unixuser.org>
2014-01-07 19:05:08 +0900

    add some plugins and dependencies.
    http://z.caudate.me/give-your-clojure-workflow-more-flow/

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,13 @@
-{:user {:plugins [[lein-ancient    "0.5.4"]
-                  [lein-bikeshed   "0.1.3"]
+{:user {:plugins [[codox "0.6.6"]
+                  [lein-ancient    "0.5.4"]
+                  [lein-bikeshed   "0.1.4"]
                   [lein-cloverage  "1.0.2"]
                   [lein-difftest   "2.0.0"]
                   [lein-licenses   "0.1.1"]
-                  [lein-localrepo  "0.5.2"]
+                  [lein-localrepo  "0.5.3"]
                   [lein-marginalia "0.7.1"]
+                  [lein-midje    "3.1.3"]
+                  [lein-midje-doc "0.0.18"]
                   [lein-kibit      "0.0.8"]
                   [lein-pprint     "1.1.1"]
                   [lein-try        "0.4.1"]
@@ -12,5 +15,24 @@
                   [perforate       "0.3.3"]]
         :search-page-size 30
         :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                       [jonase/kibit "0.0.8"]]
+                       [im.chit/vinyasa "0.1.7"]
+                       [io.aviso/pretty "0.1.8"]
+                       [jonase/kibit "0.0.8"]
+                       [spyscope "0.1.4"]]
+        :injections  [(require 'spyscope.core)
+                      (require 'vinyasa.inject)
+                      (vinyasa.inject/inject 'clojure.core
+                                             '[[vinyasa.inject [inject inject]]
+                                               [vinyasa.pull [pull pull]]
+                                               [vinyasa.lein [lein lein]]
+                                               [clojure.tools.namespace.repl [refresh refresh]]
+                                               [clojure.pprint [pprint >pprint]]
+                                               [io.aviso.binary [write-binary >bin]]])
+                      (require 'io.aviso.repl
+                               'clojure.repl
+                               'clojure.main)
+                      (alter-var-root #'clojure.main/repl-caught
+                                      (constantly @#'io.aviso.repl/pretty-pst))
+                      (alter-var-root #'clojure.repl/pst
+                                      (constantly @#'io.aviso.repl/pretty-pst))]
         :jvm-opts ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]}}
