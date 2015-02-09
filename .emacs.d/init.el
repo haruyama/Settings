8dfb32180b40dc3de830f4408c003cb1fd89573c
HARUYAMA Seigo <haruyama@unixuser.org>
2015-02-09 16:29:52 +0900

    Add help setting
    http://d.hatena.ne.jp/a_bicky/20140104/1388822688

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,19 +37,48 @@
     (dolist (package not-installed)
       (package-install package))))
 
-(progn
-  (require 'helm-config)
+(when  (require 'helm-config nil t)
   (require 'helm-ls-git)
   (helm-mode 1)
 
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
   (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
-                                        ;(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-map  (kbd  "C-h") 'delete-backward-char)
   (define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
 
-  (custom-set-variables
-   `(helm-truncate-lines t)))
+  (custom-set-variables `(helm-truncate-lines t))
+  
+  ;; Disable helm in some functions
+  (add-to-list 'helm-completing-read-handlers-alist '(find-alternate-file . nil))
+
+  ;; Emulate `kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
+
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it))
+
+  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
+    "Transform the pattern to reflect my intention"
+    (let* ((pattern (ad-get-arg 0))
+           (input-pattern (file-name-nondirectory pattern))
+           (dirname (file-name-directory pattern)))
+      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
+      (setq ad-return-value
+            (concat dirname
+                    (if (string-match "^\\^" input-pattern)
+                        ;; '^' is a pattern for basename
+                        ;; and not required because the directory name is prepended
+                        (substring input-pattern 1)
+                      (concat ".*" input-pattern))))))
+  )
 
 (require 'auto-async-byte-compile)
 (defvar auto-async-byte-complile-exclude-files-regexp "/junk/")
