f7cde9ee5e976ccdbc5b921e84b1bc3602b63da1
HARUYAMA Seigo <haruyama@unixuser.org>
2018-08-02 13:20:29 +0900

    Remove marmalade from package-archives

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ;("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (require 'generic-x)
