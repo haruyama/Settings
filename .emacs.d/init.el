0f8b240b6e6924b31d62c4dfea57cf3bd214f973
HARUYAMA Seigo <haruyama@unixuser.org>
2018-12-05 10:34:44 +0900

    Change melpa URL

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ;("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.org/packages/")
+                         ))
 
 (require 'generic-x)
 
