9ea57c473be71d75ea02f86447154872253bbfd3
HARUYAMA Seigo <haruyama@unixuser.org>
2020-05-11 10:22:04 +0900

    Use leaf.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,106 +1,91 @@
-(require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ;("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")
-                         ))
+;;; init.el --- My init.el  -*- lexical-binding: t; -*-
 
-(require 'generic-x)
+;; Copyright (C) 2020  Naoya Yamashita
 
-(require 'eieio)
-(package-initialize)
+;; Original Author: Naoya Yamashita <conao3@gmail.com>
+;; https://qiita.com/conao3/items/347d7e472afd0c58fbd7
 
-(require `cl-lib)
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-(defvar installing-package-list
-  '(apel
-    auto-complete
-    auto-save-buffers-enhanced
-    auto-async-byte-compile
-    cp5022x
-    ddskk
-    flim
-    flymake
-    fuzzy
-;    helm
-;    helm-ls-git
-    magit
-    markdown-mode
-    mu-cite
-    paredit
-    popup
-    recentf-ext
-    semi
-    session
-    wanderlust))
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-;; refresh package list if it is not already available
-(when (not package-archive-contents) (package-refresh-contents))
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; install packages from the list that are not yet installed
-(dolist (pkg installing-package-list)
-  (when (and (not (package-installed-p pkg)) (assoc pkg package-archive-contents))
-    (package-install pkg)))
+;;; Commentary:
 
-;(when  (require 'helm-config nil t)
-;  (require 'helm-ls-git)
-;  (helm-mode 1)
+;; My init.el.
 
-;  (global-set-key (kbd "M-x") 'helm-M-x)
-;  (global-set-key (kbd "C-x C-f") 'helm-find-files)
-;  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
-;  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-;  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
-;  (define-key helm-map  (kbd  "C-h") 'delete-backward-char)
-;  (define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+;;; Code:
 
-;  (custom-set-variables `(helm-truncate-lines t))
+;; this enables this running method
+;;   emacs -q -l ~/.debug.emacs.d/{{pkg}}/init.el
 
-  ; Disable helm in some functions
-;  (add-to-list 'helm-completing-read-handlers-alist '(find-alternate-file . nil))
+(add-to-list 'load-path "~/.emacs.d/elisp")
 
-  ; Emulate `kill-line' in helm minibuffer
-;  (setq helm-delete-minibuffer-contents-from-point t)
-;  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
-;    "Emulate `kill-line' in helm minibuffer"
-;    (kill-new (buffer-substring (point) (field-end))))
+(eval-and-compile
+  (when (or load-file-name byte-compile-current-file)
+    (setq user-emacs-directory
+          (expand-file-name
+           (file-name-directory (or load-file-name byte-compile-current-file))))))
 
-;  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
-;    "Execute command only if CANDIDATE exists"
-;    (when (file-exists-p candidate)
-;      ad-do-it))
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org"   . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu"   . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
 
-;  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
-;    "Transform the pattern to reflect my intention"
-;    (let* ((pattern (ad-get-arg 0))
-;           (input-pattern (file-name-nondirectory pattern))
-;           (dirname (file-name-directory pattern)))
-;      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
-;      (setq ad-return-value
-;            (concat dirname
-;                    (if (string-match "^\\^" input-pattern)
-                        ; '^' is a pattern for basename
-                        ; and not required because the directory name is prepended
-;                        (substring input-pattern 1)
-;                      (concat ".*" input-pattern))))))
-;  )
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
 
-(require 'auto-async-byte-compile)
-(defvar auto-async-byte-complile-exclude-files-regexp "/junk/")
-(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
 
-(blink-cursor-mode 0)
-(column-number-mode t)
-(display-time-mode t)
-(global-font-lock-mode t)
-(line-number-mode t)
+(leaf leaf
+  :config
+  (leaf leaf-convert :ensure t)
+  (leaf leaf-tree
+    :ensure t
+    :custom ((imenu-list-size . 30)
+             (imenu-list-position . 'left))))
 
-(savehist-mode t)
-(setq-default save-place t)
-(show-paren-mode t)
-(defvar who-paren-style 'mixed)
-(transient-mark-mode t)
+(leaf macrostep
+  :ensure t
+  :bind (("C-c e" . macrostep-expand)))
 
-(setq-default tab-width 2 indent-tabs-mode nil)
+(leaf cus-edit
+  :doc "tools for customizing Emacs and Lisp packages"
+  :tag "builtin" "faces" "help"
+  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
+
+(leaf auto-complete)
+(leaf ddskk
+      :config (load "HS_skk-setup"))
+(leaf flymake)
+(leaf mu-cite)
+(leaf mu-cite)
+(leaf recentf)
+(leaf recentf-ext)
+(leaf wl)
+
+(keyboard-translate ?\C-h ?\C-?)
+(define-key ctl-x-map "L" 'goto-line)
 
 (setq completion-ignore-case t)
 (setq echo-keystrokes 1000)
@@ -116,164 +101,6 @@
 (setq require-final-newline t)
 (setq use-dialog-box nil)
 
-(defalias 'message-box 'message)
+(provide 'init)
 
-(defadvice abort-recursive-edit (before minibuffer-save activate)
-  (when (eq (selected-window) (active-minibuffer-window))
-    (add-to-history minibuffer-history-variable (minibuffer-contents))))
-
-(ffap-bindings)
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style `post-forward-angle-brackets)
-(setq uniquify-ignore-buffers-re "*[^*]+*")
-
-                                        ;key binding
-(keyboard-translate ?\C-h ?\C-?)
-                                        ;(define-key global-map "\C-h" 'backward-delete-char)
-(define-key global-map "\C-x\C-b"  'electric-buffer-list)
-(define-key global-map "\C-x\C-m"  'newline-and-indent)
-(define-key ctl-x-map "L" 'goto-line)
-
-(recentf-mode)
-(defvar recentf-max-saved-items 500)
-(defvar recentf-exclude '("/TAGS$" "/var/tmp"))
-(require 'recentf-ext)
-(define-key global-map "\C-z"  'recentf-open-files)
-
-(require 'flymake)
-(set-face-background 'flymake-errline "red4")
-(set-face-background 'flymake-warnline "dark slate blue")
-
-(require 'auto-complete-config)
-(global-auto-complete-mode t)
-(add-to-list 'ac-modes 'text-mode)
-(add-to-list 'ac-modes 'oz-mode)
-(setq ac-ignore-case t)
-(setq ac-use-menu-map t)
-;; デフォルトで設定済み
-(define-key ac-menu-map "\C-n" 'ac-next)
-(define-key ac-menu-map "\C-p" 'ac-previous)
-
-;; 整形済み
-(setq-default ac-sources '(ac-source-words-in-same-mode-buffers
-                           ac-source-filename
-                           ac-source-functions
-                           ac-source-yasnippet
-                           ac-source-variables
-                           ac-source-symbols
-                           ac-source-features
-                           ac-source-abbrev
-                           ac-source-dictionary
-                           ac-source-words-in-all-buffer))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(mime-view-type-subtype-score-alist
-   (quote
-    (((text . enriched)
-      . 3)
-     ((text . richtext)
-      . 2)
-     ((text . plain)
-      . 4)
-     ((text . html)
-      . mime-view-text/html-entity-score)
-     (multipart . mime-view-multipart-entity-score))))
- '(package-selected-packages
-   (quote
-    (wanderlust session recentf-ext paredit markdown-mode magit helm-ls-git fuzzy ddskk cp5022x auto-save-buffers-enhanced auto-complete auto-async-byte-compile)))
- '(riece-desktop-notify-message-function (quote riece-message-text))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 139 :width normal :foundry "unknown" :family "VL Gothic")))))
-
-(if (display-graphic-p)
-    (progn
-      (set-mouse-color "maroon4")
-      (set-cursor-color "wheat4")
-      ;;モードライン
-      (set-scroll-bar-mode 'right)
-
-      (setq initial-frame-alist
-            (append (list
-                     '(width . 80) ;; ウィンドウ幅
-                     '(height . 40) ;; ウィンドウの高さ
-                     )
-                    initial-frame-alist))))
-(setq default-frame-alist initial-frame-alist)
-
-(global-set-key "\M-/" 'hippie-expand)
-(setq hippie-expand-try-functions-list
-      '(try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-abbrev
-        try-expand-all-abbrevs
-        try-expand-list try-expand-line
-        try-expand-dabbrev
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-complete-lisp-symbol-partially
-        try-complete-lisp-symbol))
-
-(add-to-list 'load-path "~/.emacs.d/elisp")
-
-(load "HS_gosh")
-(when  (require 'skk nil t)
-  (load "HS_skk-setup"))
-(load "text-adjust")
-(defvar text-adjust-hankaku-except "　？！＠ー〜, , . . （）")
-
-(autoload 'wl "wl" "Wanderlust" t)
-(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
-(autoload 'wl-draft "wl" "Write draft with Wanderlust." t)
-(load "HS_mu-cite-setup")
-                                        ;(load "HS_mailcrypt-setup")
-
-(defvar pgp-version 'gpg)
-(defvar mime-pgp-command "gpg")
-(defvar pgg-default-scheme 'gpg)
-(defvar pgg-scheme 'gpg)
-(defvar pgg-cache-passphrase t)
-(defvar pgg-gpg-use-agent t)
-
-(autoload 'riece "riece" nil t)
-
-;; http://www.emacswiki.org/emacs/SavePlace
-(when (require 'saveplace nil t)
-  (setq-default save-place t)
-  (defvar save-place-file "~/.emacs.d/saved-places"))
-
-(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
-
-(require 'paredit)
-(dolist (mode '(oz-mode
-                clojure-mode
-                emacs-lisp-mode
-                scheme-mode
-                eval-expression-minibuffer-setup
-                ielm-mode
-                lisp-mode
-                lisp-interaction-mode))
-  (add-hook (intern (format "%s-hook" mode))
-            (lambda ()
-              (add-to-list (make-local-variable 'paredit-space-for-delimiter-predicates)
-                           (lambda (a b) nil))
-              (enable-paredit-mode))))
-
-(add-hook 'oz-mode-hook
-          (lambda ()
-            (define-key paredit-mode-map (kbd "{") 'paredit-open-curly)
-            (define-key paredit-mode-map (kbd "}") 'paredit-close-curly)))
-
-
-(require 'diff-mode)
-
+;; init.el ends here
