f2222a46f7fe6fa08c562feb688aa04a8dfbf967
HARUYAMA Seigo <haruyama@unixuser.org>
2025-02-26 14:20:50 +0900

    feat: show virtual_text

--- .vim/my/plugins/nvim-lspconfig.lua
+++ .vim/my/plugins/nvim-lspconfig.lua
@@ -10,6 +10,13 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
   },
 })
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  float = { border = "rounded" }
+})
+
 vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
