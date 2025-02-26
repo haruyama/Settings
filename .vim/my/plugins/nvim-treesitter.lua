19d3693dad927c3586b017cb238678889f560c62
HARUYAMA Seigo <haruyama@unixuser.org>
2025-02-26 14:21:09 +0900

    feat: set highlight.additional_vim_regex_highlighting to true

--- .vim/my/plugins/nvim-treesitter.lua
+++ .vim/my/plugins/nvim-treesitter.lua
@@ -2,6 +2,7 @@ require('nvim-treesitter.configs').setup {
     ensure_installed = 'all',
     highlight = {
         enable = true,
+        additional_vim_regex_highlighting = true,
     },
     indent = {
         enable = true,
