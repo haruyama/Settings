1b52cb2c46144165f31b7caefff3588b78a56bc1
HARUYAMA Seigo <haruyama@unixuser.org>
2025-03-15 11:38:40 +0900

    deps: add ggandor/leap.nvim and ggandor/flit.nvim

---
+++ .vim/my/plugins/leap.nvim.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', 's', '<Plug>(leap-anywhere)')
+vim.keymap.set('x', 's', '<Plug>(leap)')
+vim.keymap.set('o', 's', '<Plug>(leap-forward)')
+vim.keymap.set('o', 'S', '<Plug>(leap-backward)')
