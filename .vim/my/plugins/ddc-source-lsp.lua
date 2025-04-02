8e0863e6ded17f70a769c934b3c92587690f6357
HARUYAMA Seigo <haruyama@unixuser.org>
2025-04-02 14:28:58 +0900

    client.supports_method() is deprecated

--- .vim/my/plugins/ddc-source-lsp.lua
+++ .vim/my/plugins/ddc-source-lsp.lua
@@ -22,7 +22,7 @@ local on_attach = function(client, bufnr)
     -- キーマッピングでシグネチャヘルプを呼び出す
     vim.api.nvim_buf_set_keymap(bufnr, "i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
 
-    if client.supports_method("textDocument/inlayHint") or client.server_capabilities.inlayHintProvider then
+    if client:supports_method("textDocument/inlayHint") or client.server_capabilities.inlayHintProvider then
         vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
     end
 end
