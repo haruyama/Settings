5cb8439e26cf094c0dd36f87e40103836d768f95
HARUYAMA Seigo <haruyama@unixuser.org>
2025-07-07 23:13:29 +0900

    chore: indent

--- _config/nvim/after/ftplugin/gitcommit.lua
+++ _config/nvim/after/ftplugin/gitcommit.lua
@@ -1,31 +1,29 @@
-
 local function select_type()
-  -- 現在のカーソル位置の行を取得
-  local line = vim.api.nvim_get_current_line()
+    -- 現在のカーソル位置の行を取得
+    local line = vim.api.nvim_get_current_line()
 
-  -- 先頭の '#' と空白を除去
-  line = line:gsub("^#%s*", "")
+    -- 先頭の '#' と空白を除去
+    line = line:gsub("^#%s*", "")
 
-  -- 最初の単語を取得
-  local word = vim.split(line, "%s+")[1] or ""
-  local title = word .. ": "
+    -- 最初の単語を取得
+    local word = vim.split(line, "%s+")[1] or ""
+    local title = word .. ": "
 
-  local bufnr = vim.api.nvim_get_current_buf()
+    local bufnr = vim.api.nvim_get_current_buf()
 
-  -- バッファ全体を削除
-  local line_count = vim.api.nvim_buf_line_count(bufnr)
-  vim.api.nvim_buf_set_lines(bufnr, 0, line_count, false, {})
+    -- バッファ全体を削除
+    local line_count = vim.api.nvim_buf_line_count(bufnr)
+    vim.api.nvim_buf_set_lines(bufnr, 0, line_count, false, {})
 
-  -- title を挿入
-  vim.api.nvim_buf_set_lines(bufnr, 0, 0, false, { title })
+    -- title を挿入
+    vim.api.nvim_buf_set_lines(bufnr, 0, 0, false, { title })
 
-  -- 最初の行・最後の文字へ移動
-  vim.api.nvim_win_set_cursor(0, { 1, #title })
+    -- 最初の行・最後の文字へ移動
+    vim.api.nvim_win_set_cursor(0, { 1, #title })
 
-  -- インサートモードに入る
-  vim.api.nvim_feedkeys("a", "n", false)
+    -- インサートモードに入る
+    vim.api.nvim_feedkeys("a", "n", false)
 end
 
 -- バッファローカルに <CR><CR> を割り当て
 vim.keymap.set("n", "<CR><CR>", select_type, { buffer = true, desc = "Select type and clear buffer" })
-
